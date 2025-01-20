f1d807c5bdd9fa8edc553c6e2d0c9a454a5690d7
LR-OM <Lauraromero772@gmail.com>
2025-01-20 03:11:53 -0600

    cleaned my js code deleted the prompt of "enter a city" and the alerts

--- Script.js
+++ Script.js
@@ -1,19 +1,3 @@
-let city = prompt("Enter a city?");
-city = city.toLowerCase();
-if (weather[city] !== undefined) {
-  let temperature = weather[city].temp;
-  let humidity = weather[city].humidity;
-  let celsiusTemperature = Math.round(temperature);
-  let fahrenheitTemperature = Math.round((temperature * 9) / 5 + 32);
-
-  alert(
-    "It is currently ${celsiusTemperature}°C (${fahrenheitTemperature}°F) in ${city} with a humidity of ${humidity}%"
-  );
-} else {
-  alert(
-    "Sorry we don't know the weather for this city, try going to https://www.google.com/search?q=weather+${city}"
-  );
-}
 function search(event) {
   event.preventDefault();
   let searchInputElement = document.querySelector("#search-input");
